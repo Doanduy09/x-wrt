917742045574cd2bd7282ba91e4994216fafc99a
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2025-01-29 07:10:01 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,8 +25,10 @@ rm -rf feeds/packages/net/quectel-cm
 rm -rf feeds/packages/quectel-cm
 rm -rf feeds/packages/kernel/fibocom-qmi-wwan/
 rm -rf feeds/packages/kernel/quectel-qmi-wwan/
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds update -a; ./scripts/feeds install -a
-
+./scripts/feeds install -a 
 
